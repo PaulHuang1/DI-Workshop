44a0edd8071088b7ec8efdf804812f36b5f74f76
Kevin Liao <kevin.liao@mail.aircast.biz>
2019-05-04 23:33:57 +0800

    Refactor for test case

--- DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
+++ DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
@@ -10,48 +10,88 @@ namespace DependencyInjectionWorkshopTests
     [TestFixture]
     public class AuthenticationServiceTests
     {
+        private const string DefaultAccount = "joey";
+        private const string DefaultHashedPassword = "my hashed password";
+        private const string DefaultOtp = "123456";
+        private const string DefaultPassword = "pw";
+        private AuthenticationService _authenticationService;
+        private IFailedCounter _failedCounter;
+        private IHash _hash;
+        private ILogger _logger;
+        private INotification _notification;
+        private IOtp _otp;
+        private IProfile _profile;
+
         [Test]
         public void is_invalid_when_wrong_otp()
         {
-            var failedCounter = Substitute.For<IFailedCounter>();
-            var logger = Substitute.For<ILogger>();
-            var profile = Substitute.For<IProfile>();
-            var hash = Substitute.For<IHash>();
-            var otp = Substitute.For<IOtp>();
-            var notification = Substitute.For<INotification>();
+            _profile.GetPassword(DefaultAccount).ReturnsForAnyArgs(DefaultHashedPassword);
+            _hash.GetHash(DefaultPassword).ReturnsForAnyArgs(DefaultHashedPassword);
+            _otp.GetOtp(DefaultAccount).ReturnsForAnyArgs(DefaultOtp);
 
-            var authenticationService =
-                new AuthenticationService(failedCounter, logger, otp, profile, hash, notification);
+            var isValid = WhenVerify(DefaultAccount, DefaultPassword, "wrong otp");
 
-            profile.GetPassword("joey").ReturnsForAnyArgs("my hashed password");
-            hash.GetHash("pw").ReturnsForAnyArgs("my hashed password");
-            otp.GetOtp("joey").ReturnsForAnyArgs("123456");
-
-            var isValid = authenticationService.Verify("joey", "pw", "wrong otp");
-
-            Assert.IsFalse(isValid);
+            ShouldBeInvalid(isValid);
         }
 
         [Test]
         public void is_valid()
         {
-            var failedCounter = Substitute.For<IFailedCounter>();
-            var logger = Substitute.For<ILogger>();
-            var profile = Substitute.For<IProfile>();
-            var hash = Substitute.For<IHash>();
-            var otp = Substitute.For<IOtp>();
-            var notification = Substitute.For<INotification>();
+            _profile.GetPassword(DefaultAccount).ReturnsForAnyArgs(DefaultHashedPassword);
+            _hash.GetHash(DefaultPassword).ReturnsForAnyArgs(DefaultHashedPassword);
+            _otp.GetOtp(DefaultAccount).ReturnsForAnyArgs(DefaultOtp);
 
-            var authenticationService =
-                new AuthenticationService(failedCounter, logger, otp, profile, hash, notification);
+            var isValid = WhenVerify(DefaultAccount, DefaultPassword, DefaultOtp);
 
-            profile.GetPassword("joey").ReturnsForAnyArgs("my hashed password");
-            hash.GetHash("pw").ReturnsForAnyArgs("my hashed password");
-            otp.GetOtp("joey").ReturnsForAnyArgs("123456");
+            ShouldBeValid(isValid);
+        }
 
-            var isValid = authenticationService.Verify("joey", "pw", "123456");
+        [SetUp]
+        public void Setup()
+        {
+            _failedCounter = Substitute.For<IFailedCounter>();
+            _logger = Substitute.For<ILogger>();
+            _profile = Substitute.For<IProfile>();
+            _hash = Substitute.For<IHash>();
+            _otp = Substitute.For<IOtp>();
+            _notification = Substitute.For<INotification>();
 
+            _authenticationService = new AuthenticationService(_failedCounter, _logger, _otp, _profile, _hash, _notification);
+        }
+
+        private static void ShouldBeInvalid(bool isValid)
+        {
+            Assert.IsFalse(isValid);
+        }
+
+        private static void ShouldBeValid(bool isValid)
+        {
             Assert.IsTrue(isValid);
+        }
+
+        private bool WhenInvalid()
+        {
+            _profile.GetPassword(DefaultAccount).ReturnsForAnyArgs(DefaultHashedPassword);
+            _hash.GetHash(DefaultPassword).ReturnsForAnyArgs(DefaultHashedPassword);
+            _otp.GetOtp(DefaultAccount).ReturnsForAnyArgs(DefaultOtp);
+
+            var isValid = WhenVerify(DefaultAccount, DefaultPassword, "wrong otp");
+            return isValid;
+        }
+
+        private bool WhenValid()
+        {
+            _profile.GetPassword(DefaultAccount).ReturnsForAnyArgs(DefaultHashedPassword);
+            _hash.GetHash(DefaultPassword).ReturnsForAnyArgs(DefaultHashedPassword);
+            _otp.GetOtp(DefaultAccount).ReturnsForAnyArgs(DefaultOtp);
+
+            var isValid = WhenVerify(DefaultAccount, DefaultPassword, DefaultOtp);
+            return isValid;
+        }
+
+        private bool WhenVerify(string account, string password, string otp)
+        {
+            return _authenticationService.Verify(account, password, otp);
         }
     }
 }
