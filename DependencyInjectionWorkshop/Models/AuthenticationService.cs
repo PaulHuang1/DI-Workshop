9a8ed02a3fef36274eb8df031847db0726ee13a7
Kevin Liao <kevin.liao@mail.aircast.biz>
2019-05-04 11:23:12 +0800

    extract methods, composed method

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -19,6 +19,39 @@ namespace DependencyInjectionWorkshop.Models
             {
                 BaseAddress = otpApiUri
             };
+            CheckAccountIsLocked(account, httpClient);
+
+            var passwordFromDb = GetPasswordFromDb(account);
+
+            var hashPassword = GetHashPassword(password);
+
+            var currentOtp = GetCurrentOtp(account, httpClient);
+
+            if (passwordFromDb == hashPassword && otp == currentOtp)
+            {
+                ResetFailedCount(account, httpClient);
+
+                return true;
+            }
+
+            AddFailedCount(account, httpClient);
+
+            var failedCount = GetFailedCount(account, httpClient);
+            LogFailedCount($"account:{account} verify failed! Current failed times is {failedCount}");
+
+            Notify($"account:{account} verify failed");
+
+            return false;
+        }
+
+        private static void AddFailedCount(string account, HttpClient httpClient)
+        {
+            var response = httpClient.PostAsJsonAsync("api/failedCounter/Add", account).Result;
+            response.EnsureSuccessStatusCode();
+        }
+
+        private static void CheckAccountIsLocked(string account, HttpClient httpClient)
+        {
             var response = httpClient.PostAsJsonAsync("api/failedCounter/IsLocked", account).Result;
             response.EnsureSuccessStatusCode();
             var isLocked = response.Content.ReadAsAsync<bool>().Result;
@@ -26,7 +59,41 @@ namespace DependencyInjectionWorkshop.Models
             {
                 throw new FailedTooManyTimeException();
             }
+        }
 
+        private static string GetCurrentOtp(string account, HttpClient httpClient)
+        {
+            var response = httpClient.PostAsJsonAsync("api/otps", account).Result;
+            var currentOtp = response.IsSuccessStatusCode
+                ? response.Content.ReadAsAsync<string>().Result
+                : throw new Exception($"Get OTP web api error, accountId:{account}");
+            return currentOtp;
+        }
+
+        private static int GetFailedCount(string account, HttpClient httpClient)
+        {
+            var response = httpClient.PostAsJsonAsync("api/failedCounter/GetFailedCount", account).Result;
+            response.EnsureSuccessStatusCode();
+            var failedCount = response.Content.ReadAsAsync<int>().Result;
+            return failedCount;
+        }
+
+        private static string GetHashPassword(string password)
+        {
+            var crypt = new SHA256Managed();
+            var hash = new StringBuilder();
+            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(password));
+            foreach (var theByes in crypto)
+            {
+                hash.Append(theByes.ToString("x2"));
+            }
+
+            var hashPassword = hash.ToString();
+            return hashPassword;
+        }
+
+        private static string GetPasswordFromDb(string account)
+        {
             string profilePassword;
             var connectionString = "my connection string";
             var spName = "spGetUserPassword";
@@ -37,42 +104,25 @@ namespace DependencyInjectionWorkshop.Models
                     commandType: CommandType.StoredProcedure).SingleOrDefault();
             }
 
-            var crypt = new SHA256Managed();
-            var hash = new StringBuilder();
-            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(password));
-            foreach (var theByes in crypto)
-            {
-                hash.Append(theByes.ToString("x2"));
-            }
-            var hashPassword = hash.ToString();
+            return profilePassword;
+        }
 
-            response = httpClient.PostAsJsonAsync("api/otps", account).Result;
-            var currentOtp = response.IsSuccessStatusCode
-                ? response.Content.ReadAsAsync<string>().Result
-                : throw new Exception($"Get OTP web api error, accountId:{account}");
+        private static void LogFailedCount(string message)
+        {
+            var logger = NLog.LogManager.GetCurrentClassLogger();
+            logger.Info(message);
+        }
 
-            if (profilePassword == hashPassword && otp == currentOtp)
-            {
-                response = httpClient.PostAsJsonAsync("api/failedCounter/Reset", account).Result;
-                response.EnsureSuccessStatusCode();
-
-                return true;
-            }
-
-            var message = $"account:{account} verify failed";
+        private static void Notify(string message)
+        {
             var slackClient = new SlackClient("my api token");
             slackClient.PostMessage(resp => { }, "my channel", message, "my bot name");
+        }
 
-            response = httpClient.PostAsJsonAsync("api/failedCounter/Add", account).Result;
+        private static void ResetFailedCount(string account, HttpClient httpClient)
+        {
+            var response = httpClient.PostAsJsonAsync("api/failedCounter/Reset", account).Result;
             response.EnsureSuccessStatusCode();
-
-            response = httpClient.PostAsJsonAsync("api/failedCounter/GetFailedCount", account).Result;
-            response.EnsureSuccessStatusCode();
-            var failedCount = response.Content.ReadAsAsync<int>().Result;
-            var logger = NLog.LogManager.GetCurrentClassLogger();
-            logger.Info($"account:{account} verify failed! Current failed times is {failedCount}");
-
-            return false;
         }
     }
 
