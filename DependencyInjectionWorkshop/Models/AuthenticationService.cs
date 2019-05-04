3472f8e460e348edff1844ae1cad2201419b729c
Kevin Liao <kevin.liao@mail.aircast.biz>
2019-05-04 21:05:40 +0800

    Implement AuthenticationService Verify

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -1,6 +1,48 @@
-﻿namespace DependencyInjectionWorkshop.Models
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using Dapper;
+
+namespace DependencyInjectionWorkshop.Models
 {
     public class AuthenticationService
     {
+        public bool Verify(string account, string password, string otp)
+        {
+            string passwordFromDb;
+            using (var connection = new SqlConnection("my connection string"))
+            {
+                passwordFromDb = connection.Query<string>("spGetUserPassword",
+                        new { Account = account },
+                        commandType: CommandType.StoredProcedure)
+                    .SingleOrDefault();
+            }
+
+            string hashedPassword;
+            using (var crypt = new SHA256Managed())
+            {
+                var builder = new StringBuilder();
+                var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(password));
+                foreach (var theByte in crypto)
+                {
+                    builder.Append(theByte.ToString("x2"));
+                }
+                hashedPassword = builder.ToString();
+            }
+
+            string currentOtp;
+            using (var httpClient = new HttpClient { BaseAddress = new Uri("http://joey.dev") })
+            {
+                var response = httpClient.PostAsJsonAsync("api/otps", account).Result;
+                response.EnsureSuccessStatusCode();
+                currentOtp = response.Content.ReadAsAsync<string>().Result;
+            }
+
+            return passwordFromDb == hashedPassword && otp == currentOtp;
+        }
     }
 }
