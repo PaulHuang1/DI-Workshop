083150de233dfaad065c1f537aa2084f81095693
Kevin Liao <kevin.liao@mail.aircast.biz>
2019-05-04 23:21:35 +0800

    Add is_invalid_when_wrong_otp test case

--- DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
+++ DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
@@ -11,6 +11,28 @@ namespace DependencyInjectionWorkshopTests
     public class AuthenticationServiceTests
     {
         [Test]
+        public void is_invalid_when_wrong_otp()
+        {
+            var failedCounter = Substitute.For<IFailedCounter>();
+            var logger = Substitute.For<ILogger>();
+            var profile = Substitute.For<IProfile>();
+            var hash = Substitute.For<IHash>();
+            var otp = Substitute.For<IOtp>();
+            var notification = Substitute.For<INotification>();
+
+            var authenticationService =
+                new AuthenticationService(failedCounter, logger, otp, profile, hash, notification);
+
+            profile.GetPassword("joey").ReturnsForAnyArgs("my hashed password");
+            hash.GetHash("pw").ReturnsForAnyArgs("my hashed password");
+            otp.GetOtp("joey").ReturnsForAnyArgs("123456");
+
+            var isValid = authenticationService.Verify("joey", "pw", "wrong otp");
+
+            Assert.IsFalse(isValid);
+        }
+
+        [Test]
         public void is_valid()
         {
             var failedCounter = Substitute.For<IFailedCounter>();
