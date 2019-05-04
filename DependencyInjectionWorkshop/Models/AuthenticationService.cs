09109bba8f402e9919b5d85a2a010b54b8e9b1c1
Joey Chen <jay.hatelove@gmail.com>
2019-05-04 11:55:56 +0800

    move class to independent files

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -1,112 +1,8 @@
-﻿using System;
-using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using Dapper;
-using SlackAPI;
+﻿using DependencyInjectionWorkshop.Adapters;
+using DependencyInjectionWorkshop.Repositories;
 
 namespace DependencyInjectionWorkshop.Models
 {
-    public class ProfileRepo
-    {
-        public string GetPasswordFromDb(string accountId)
-        {
-            string passwordFromDb;
-            using (var connection = new SqlConnection("my connection string"))
-            {
-                passwordFromDb = connection.Query<string>("spGetUserPassword", new {Id = accountId},
-                    commandType: CommandType.StoredProcedure).SingleOrDefault();
-            }
-
-            return passwordFromDb;
-        }
-    }
-
-    public class Sha256Adapter
-    {
-        public string GetHashedPassword(string password)
-        {
-            var crypt = new System.Security.Cryptography.SHA256Managed();
-            var hash = new StringBuilder();
-            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(password));
-            foreach (var theByte in crypto)
-            {
-                hash.Append(theByte.ToString("x2"));
-            }
-
-            var hashedPassword = hash.ToString();
-            return hashedPassword;
-        }
-    }
-
-    public class OtpService
-    {
-        public string GetCurrentOtp(string accountId)
-        {
-            var response = new HttpClient() {BaseAddress = new Uri("http://joey.dev/")}
-                .PostAsJsonAsync("api/otps", accountId).Result;
-            string currentOtp;
-            if (response.IsSuccessStatusCode)
-            {
-                currentOtp = response.Content.ReadAsAsync<string>().Result;
-            }
-            else
-            {
-                throw new Exception($"web api error, accountId:{accountId}");
-            }
-
-            return currentOtp;
-        }
-    }
-
-    public class FailedCounter
-    {
-        public void ResetFailedCounter(string accountId)
-        {
-            var resetResponse = new HttpClient() {BaseAddress = new Uri("http://joey.dev/")}
-                .PostAsJsonAsync("api/failedCounter/Reset", accountId).Result;
-            resetResponse.EnsureSuccessStatusCode();
-        }
-
-        public void AddFailedCount(string accountId)
-        {
-            var addFailedCountResponse = new HttpClient() {BaseAddress = new Uri("http://joey.dev/")}
-                .PostAsJsonAsync("api/failedCounter/Add", accountId).Result;
-            addFailedCountResponse.EnsureSuccessStatusCode();
-        }
-
-        public int GetFailedCount(string accountId)
-        {
-            var failedCountResponse =
-                new HttpClient() {BaseAddress = new Uri("http://joey.dev/")}
-                    .PostAsJsonAsync("api/failedCounter/GetFailedCount", accountId).Result;
-            failedCountResponse.EnsureSuccessStatusCode();
-
-            var failedCount = failedCountResponse.Content.ReadAsAsync<int>().Result;
-            return failedCount;
-        }
-    }
-
-    public class NLogAdapter
-    {
-        public void LogFailedCount(string message)
-        {
-            var logger = NLog.LogManager.GetCurrentClassLogger();
-            logger.Info(message);
-        }
-    }
-
-    public class SlackAdapter
-    {
-        public void Notify(string message)
-        {
-            var slackClient = new SlackClient("my api token");
-            slackClient.PostMessage(r => { }, "my channel", message, "my bot name");
-        }
-    }
-
     public class AuthenticationService
     {
         private readonly ProfileRepo _profileRepo = new ProfileRepo();
@@ -118,7 +14,7 @@ namespace DependencyInjectionWorkshop.Models
 
         public bool Verify(string accountId, string password, string otp)
         {
-            CheckAccountIsLocked(accountId);
+            _failedCounter.CheckAccountIsLocked(accountId);
 
             var passwordFromDb = _profileRepo.GetPasswordFromDb(accountId);
 
@@ -144,20 +40,5 @@ namespace DependencyInjectionWorkshop.Models
                 return false;
             }
         }
-
-        private static void CheckAccountIsLocked(string accountId)
-        {
-            var isLockedResponse = new HttpClient() {BaseAddress = new Uri("http://joey.dev/")}
-                .PostAsJsonAsync("api/failedCounter/IsLocked", accountId).Result;
-            isLockedResponse.EnsureSuccessStatusCode();
-            if (isLockedResponse.Content.ReadAsAsync<bool>().Result)
-            {
-                throw new FailedTooManyTimesException();
-            }
-        }
-    }
-
-    public class FailedTooManyTimesException : Exception
-    {
     }
 }
